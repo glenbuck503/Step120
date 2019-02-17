2aa4a7d93ba149a5295d75bd5226d290975a5a67
Glen Buck <glenbuck503@gmail.com>
2019-02-16 18:37:23 -0800

    Step120

---
+++ Step 120/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person persons = new Person();
+            persons.FirstName = "Glen";
+            persons.LastName = " Buck";
+            persons.SayName();
+
+            Employee employees = new Employee();
+            employees.FirstName = "Sample";
+            employees.LastName = " Student";
+            employees.SayName();
+
+            
+            Console.ReadLine();
+
+
+
+         
+
+        }
+    }
+}
